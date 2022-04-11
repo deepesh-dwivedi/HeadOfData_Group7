8d5dba06f8c60280d7a2fb1f72b05d01e5e2d28b
AbhishekT2408 <abhi.tiwari2402@gmail.com>
2022-04-11 18:09:03 +0200

    Changes

--- regex1.py
+++ regex1.py
@@ -1,23 +1,15 @@
 import re
 
 file = open("/Users/abhishektiwari/Documents/GitHub/HeadOfData_Group7/deliveroo/Fri_01_Nov_2019_12_00_40_.html","r")
-
-print(" step 1")
 data=file.read()
-print(" step 2")
-
 file.close()
 
 
 def regphone(x):
-    a=x
-    a=a.replace(" ","")
-    pattern = "\+33[6|7]\d{8}"
-    result =re.match(pattern, a)
-    return result
-
+    result = re.findall(r'\+33 [6|7] \d{2} \d{2} \d{2} \d{2}', x)
+    for rez in result:
+        print(rez)
 
 r = regphone(data)
 
-print(r)
 
